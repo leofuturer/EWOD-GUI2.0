7a02837c9c33de1257160bcc009ca93dc77c9ca8
OswaldHe <zifanhe1202@ucla.edu>
2021-02-13 18:17:06 -0800

    delete redux related

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {createStore, combineReducers} from 'redux';
-import {canvasReducer} from './canvas'
-import {scrollReducer} from './scroll'
-const rootReducer = combineReducers(canvasReducer, scrollReducer);
-const baseStore = createStore(rootReducer);
-export default baseStore;

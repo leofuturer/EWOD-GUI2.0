e6164c8169c41a75ae04f6968e03ff87193cb4aa
Jessica Ho <jessicaho@vpn-172-27-10-52.host.ucla.edu>
2021-02-14 13:23:53 -0800

    control panel

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import './App.css';
 import React, { Component } from "react"
-import Dexie from "dexie"
 import Provider from "./Provider";
 
 import { Canvas } from "./Canvas.js"
 import Scroll from "./Scroll.js"
-
-const numCols = 5
-const numRows = 5
-const numButtons = numCols * numRows
+import { ControlPanel } from "./ControlPanel/ControlPanel"
 
 export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      boxSelectHist: new Array(numButtons).fill(0),
-      // every box needs to keep track of whether it's been selected
-    }
-    this.combiUnselect = this.combiUnselect.bind(this)
-  }
-
-  combiUnselect(newHist) {
-    this.setState({ boxSelectHist: newHist })
-  }
-
   render() {
 
     return (
       <div className="App">
         <Provider>
-          <Canvas combiUnselect={this.combiUnselect} db={new Dexie('ElecDB')} />
-          <Scroll/>
+          <ControlPanel />
+          <Canvas />
+          <Scroll />
         </Provider>
       </div>
     )

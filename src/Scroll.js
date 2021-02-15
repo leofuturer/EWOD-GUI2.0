ebd6ed3bc58360dc2ac2e8140c2b2a170a90a56c
OswaldHe <zifanhe1202@ucla.edu>
2021-02-14 22:19:54 -0800

    remove redundancy

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext }  from 'react'
+import React, { useContext }  from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Context from "./context"

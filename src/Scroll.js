054f45607818cecf047ca362b85c810f99c84b6c
OswaldHe <zifanhe1202@ucla.edu>
2021-02-28 18:47:48 -0800

    add context menu and dialog

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,15 +1,51 @@
-import React, { useContext }  from 'react'
+import React, { useContext, useState }  from 'react'
 import {makeStyles} from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Context from "./context"
+
+const initState = {
+    mouseX : null,
+    mouseY : null,
+};
 
 export default function Scroll(props){
     const context = useContext(Context);
     const classes = useStyles();
     const {pinActuate, currentStep} = context.state;
     const {setCurrentStep} = context;
+    const [mouseState, setMouseState] = useState(initState);
+    const [open, setOpen] = useState(false);
+    const [from, setFrom] = useState("");
+    const [to, setTo] = useState("");
+    const [repTime, setRepTime] = useState("");
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setMouseState({
+            mouseX: event.clientX - 2,
+            mouseY: event.clientY - 4,
+        });
+    };
+
+    const handleClose = ()=>{
+        setMouseState(initState);
+    }
+
+    const modelOpen = () => {setOpen(true)}
+    const modelClose = () => {setOpen(false); handleClose();}
+    const changeFrom = (event) => {setFrom(event.target.value)}
+    const changeTo = (event) => {setTo(event.target.value)}
+    const changeRepTime = (event) => {setRepTime(event.target.value)}
+
     return (
-        <div className={classes.container}>
+        <div className={classes.container} onContextMenu={handleClick}>
             {Array.from(pinActuate.keys()).map(key => {
                 let value = pinActuate.get(key);
                 let append_string = "";
@@ -30,7 +66,71 @@ export default function Scroll(props){
                 setCurrentStep(pinActuate.size);
             }}>+</Button>
             <div style={{minWidth: "20px", height: "100px", backgroundColor:'transparent'}}></div>
-
+            <Menu
+                keepMounted
+                open={mouseState.mouseY !== null}
+                onClose={handleClose}
+                anchorReference="anchorPosition"
+                anchorPosition={
+                  mouseState.mouseY !== null && mouseState.mouseX !== null
+                    ? { top: mouseState.mouseY, left: mouseState.mouseX }
+                    : undefined
+                }
+            >
+                <MenuItem onClick={handleClose}>Copy</MenuItem>
+                <MenuItem onClick={handleClose}>Paste</MenuItem>
+                <MenuItem onClick={modelOpen}>Loop</MenuItem>
+                <MenuItem onClick={handleClose}>Delete</MenuItem>
+            </Menu>
+            <Dialog
+                open={open}
+                onClose={modelClose}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Set Repeat</DialogTitle>
+                <DialogContent style={{display: 'flex', flexDirection: 'column', width: 400}}>
+        
+                         <TextField
+                            autoFocus
+                            variant="outlined"
+                            label="From"
+                            style={{marginBottom: 10}}
+                            value={from}
+                            onChange={changeFrom}
+                            helperText={!isNaN(from)&&parseInt(Number(from))==from?"":"need to be a number"}
+                            error={isNaN(from)||parseInt(Number(from))!=from}
+                        />
+                  
+                         <TextField
+                            variant="outlined"
+                            label="To"
+                            style={{marginBottom: 10}}
+                            value={to}
+                            onChange={changeTo}
+                            helperText={!isNaN(to)&&parseInt(Number(to))==to?"":"need to be a number"}
+                            error={isNaN(to)||parseInt(Number(to))!=to}
+                        />
+                    
+                         <TextField
+                            variant="outlined"
+                            label="Repeat Time"
+                            style={{marginBottom: 10}}
+                            value={repTime}
+                            onChange={changeRepTime}
+                            helperText={!isNaN(repTime)&&parseInt(Number(repTime))==repTime?"":"need to be a number"}
+                            error={isNaN(repTime)||parseInt(Number(repTime))!=repTime}
+                        />
+                    
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={modelClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={modelClose} color="primary">
+                    Confirm
+                </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
@@ -68,5 +168,10 @@ const useStyles = makeStyles({
         color: 'white',
         backgroundColor: '#2a78de',
         margin:"10px"
+    },
+    modal: {
+        width: 400,
+        height: 200,
+        backgroundColor: '#778899',
     }
 });

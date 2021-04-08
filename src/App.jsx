2c9084ffac1a898c52e917092818d08b594c8308
Brian Cheng <brian.cheng@ucla.edu>
2021-04-08 16:34:12 -0700

    Style src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CanvasProvider } from './Contexts/CanvasProvider';
 import { ActuationProvider } from './Contexts/ActuationProvider';
 
 import Canvas from './Canvas/Canvas';
-import Scroll from './Scroll.js';
+import Scroll from './Scroll';
 import { ControlPanel } from './ControlPanel/ControlPanel';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ActuationProvider>
-          <CanvasProvider>
-            <ControlPanel />
-            <Canvas />
-            <Scroll />
-          </CanvasProvider>
-        </ActuationProvider>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <ActuationProvider>
+        <CanvasProvider>
+          <ControlPanel />
+          <Canvas />
+          <Scroll />
+        </CanvasProvider>
+      </ActuationProvider>
+    </div>
+  );
 }

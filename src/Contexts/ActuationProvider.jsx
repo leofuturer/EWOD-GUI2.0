79bfffa6c91b9ac0314f0504b2a278f82648a284
Brian Cheng <brian.cheng@ucla.edu>
2021-04-15 18:46:33 -0700

    ESLint autofixes

--- src/Contexts/ActuationProvider.jsx
+++ src/Contexts/ActuationProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import ActuationSequence from '../Actuation';
 
 const ActuationContext = React.createContext();
--- src/Contexts/GeneralProvider.jsx
+++ src/Contexts/GeneralProvider.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const GeneralContext = React.createContext();
 
-const GeneralProvider = (props) => {
-    const [mode, setMode] = React.useState('CAN'); // either "PIN", "SEQ", or "CAN"
-    const bannerRef = React.useRef();
+const GeneralProvider = ({ children }) => {
+  const [mode, setMode] = React.useState('CAN'); // either "PIN", "SEQ", or "CAN"
+  const bannerRef = React.useRef();
 
-    return (
-        <GeneralContext.Provider
-            value={{
-                mode,
-                setMode,
-                bannerRef
-            }}
-        >
-            {props.children}
-        </GeneralContext.Provider>
-    );
+  return (
+    <GeneralContext.Provider
+      value={{
+        mode,
+        setMode,
+        bannerRef,
+      }}
+    >
+      {children}
+    </GeneralContext.Provider>
+  );
 };
 
 export { GeneralProvider, GeneralContext };

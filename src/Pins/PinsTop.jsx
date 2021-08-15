a89bf7099939a31cf293b5bd3a8f1666c2e58389
Jessica Ho <hojessica1230@gmail.com>
2021-08-15 03:49:14 -0700

    REF map to 1 elec

--- src/Pins/PinsTop.jsx
+++ src/Pins/PinsTop.jsx
@@ -27,19 +27,19 @@ export default function PinsTop() {
         }
       </div>
       <div className="row" style={{ marginBottom: 40 }}>
-        <button className="pin ref" type="button" onClick={() => setPin(null)}>REF</button>
+        <button className="pin ref" type="button" onClick={() => setPin('REF')}>REF</button>
         {
           range(224, 218).map((pinNum, ind) => <button className="pin" type="button" key={ind.id} onClick={(e) => setPin(e.target.innerText)}>{pinNum}</button>)
         }
-        <button className="pin ref" type="button" onClick={() => setPin(null)}>REF</button>
+        <button className="pin ref" type="button" onClick={() => setPin('REF')}>REF</button>
         {
           range(217, 211).map((pinNum, ind) => <button className="pin" type="button" key={ind.id} onClick={(e) => setPin(e.target.innerText)}>{pinNum}</button>)
         }
-        <button className="pin ref" type="button" onClick={() => setPin(null)}>REF</button>
+        <button className="pin ref" type="button" onClick={() => setPin('REF')}>REF</button>
         {
           range(210, 204).map((pinNum, ind) => <button className="pin" type="button" key={ind.id} onClick={(e) => setPin(e.target.innerText)}>{pinNum}</button>)
         }
-        <button className="pin ref" type="button" onClick={() => setPin(null)}>REF</button>
+        <button className="pin ref" type="button" onClick={() => setPin('REF')}>REF</button>
         {
           range(203, 197).map((pinNum, ind) => <button className="pin" type="button" key={ind.id} onClick={(e) => setPin(e.target.innerText)}>{pinNum}</button>)
         }

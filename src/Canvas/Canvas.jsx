395cad96d4e3e02ffa194749497b430449ddf8fd
Shounak Kuiry <shounakkuiry@gmail.com>
2022-08-04 19:25:57 -0700

    Move by default when electrodes are selected

--- src/Canvas/Canvas.jsx
+++ src/Canvas/Canvas.jsx
@@ -32,6 +32,7 @@ export default function Canvas() {
   const combSelected = canvasContext.combined.selected;
   const {
     setMouseDown, setElectrodes, setSelected, setCombSelected, setComboLayout, setMoving,
+    setDragging,
   } = canvasContext;
 
   const actuationContext = useContext(ActuationContext);
@@ -386,6 +387,9 @@ export default function Canvas() {
         setSelected(sIds);
         setCombSelected(cIds);
       }
+      if (sIds.length > 0 || cIds.length > 0) {
+        setMoving(true);
+      }
     }
 
     // handle actuation
@@ -424,6 +428,8 @@ export default function Canvas() {
   }
 
   function copy() {
+    setMoving(false);
+    setDragging(false);
     const squares = [];
     const combined = [];
 
@@ -478,6 +484,8 @@ export default function Canvas() {
   }
 
   function paste(e, relX, relY) {
+    setMoving(false);
+    setDragging(false);
     if (selected.length > 0) setSelected([]);
     if (combSelected.length > 0) setCombSelected([]);
     if (!clipboard.squares && !clipboard.combined) return;
@@ -608,11 +616,15 @@ export default function Canvas() {
   }
 
   function BothDelete() {
+    setMoving(false);
+    setDragging(false);
     combinedDelete();
     squaresDelete();
   }
 
   function cut() {
+    setMoving(false);
+    setDragging(false);
     setCutFlag(true);
     copy();
     BothDelete();
@@ -632,6 +644,8 @@ export default function Canvas() {
   }
 
   function handleCombine(e) {
+    setMoving(false);
+    setDragging(false);
     e.preventDefault();
     if (selected.length < 2) {
       window.alert('You need to combine at least 2 square electrodes.');
@@ -712,6 +726,8 @@ export default function Canvas() {
   }
 
   function separate() {
+    setMoving(false);
+    setDragging(false);
     if (!combSelected.length || selected.length) {
       window.alert('Can only separate combined electrodes');
       return;
@@ -732,6 +748,8 @@ export default function Canvas() {
   }
 
   function deleteSelectedMappings() {
+    setMoving(false);
+    setDragging(false);
     if (selected.length || combSelected.length || currElec) {
       const etp = { ...elecToPin };
       const pte = { ...pinToElec };

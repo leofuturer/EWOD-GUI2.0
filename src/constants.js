f255a99734f00c78dd6356d908ff99c983a2fec8
Shounak Kuiry <shounakkuiry@gmail.com>
2022-08-17 15:35:17 -0700

    Updates height and width to match CAD dimensions

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const ELEC_SIZE = 35;
-export const CANVAS_WIDTH = 60; // electrodes
-export const CANVAS_HEIGHT = 30; // electrodes
+export const CANVAS_WIDTH = 80; // electrodes
+export const CANVAS_HEIGHT = 40; // electrodes
 export const CANVAS_TRUE_WIDTH = CANVAS_WIDTH * ELEC_SIZE;
 export const CANVAS_TRUE_HEIGHT = CANVAS_HEIGHT * ELEC_SIZE;
 

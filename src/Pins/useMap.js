b5c8319cf3b711ec93b45fd62433aae9fdb56bfe
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2023-02-03 21:25:25 -0800

    Set pins to low when they have been changed, i.e. if pin 97 is actuated, but we change the mapping of the electrode to pin 88, pin 97 will be set to low.

--- src/Pins/useMap.js
+++ src/Pins/useMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GeneralContext } from '../Contexts/GeneralProvider';
+import { setPin } from '../USBCommunication/USBCommunication';
 
 export default function useMap(callback, pin) {
   const savedCallback = React.useRef();
@@ -21,6 +22,7 @@ export default function useMap(callback, pin) {
           // e.g. pin 100 <-> electrode S3
           // then hit pin 100 again while select electrode S3
           // then set the absolute state and return immediately
+          setPin([elecToPin[currElec]], 0);
           setPinToElec((curr) => {
             const newObj = { ...curr };
             delete newObj[pin];
@@ -41,6 +43,7 @@ export default function useMap(callback, pin) {
         delete elecToPin[pinToElec[pin]];
       }
       if (Object.prototype.hasOwnProperty.call(elecToPin, currElec)) {
+        setPin([elecToPin[currElec]], 0);
         delete pinToElec[elecToPin[currElec]];
       }
 

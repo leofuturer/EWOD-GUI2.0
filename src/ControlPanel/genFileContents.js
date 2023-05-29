5fd2f4096636d2452b7b563caccaecfb4f86fb89
Shounak Kuiry <shounakkuiry@gmail.com>
2023-05-28 18:39:49 -0700

    Propagates file format change

--- src/ControlPanel/genFileContents.js
+++ src/ControlPanel/genFileContents.js
@@ -1,12 +1,13 @@
+import { ELEC_SIZE } from '../constants';
+
 export default function genFileContents(electrodes, allCombined, pinActuate, elecToPin) {
   const newContents = {}; const squares = []; const
     combs = [];
   const seq = [];
-
   if (allCombined) {
     for (let i = 0; i < allCombined.length; i += 1) {
       const comb = allCombined[i];
-      let boop = `combine ${comb[0] / 35} ${comb[1] / 35} ${comb[2]}`;
+      let boop = `combine ${comb[0] / ELEC_SIZE} ${comb[1] / ELEC_SIZE} ${comb[2]}`;
       if (elecToPin[`C${comb[2]}`]) {
         boop = `${boop} ${elecToPin[`C${comb[2]}`]}`;
       }
@@ -18,7 +19,7 @@ export default function genFileContents(electrodes, allCombined, pinActuate, ele
     for (let j = 0; j < electrodes.length; j += 1) {
       const x = electrodes[j].initPositions[0] + electrodes[j].deltas[0];
       const y = electrodes[j].initPositions[1] + electrodes[j].deltas[1];
-      let boop = `square ${x / 35} ${y / 35}`;
+      let boop = `square ${x / ELEC_SIZE} ${y / ELEC_SIZE}`;
       if (elecToPin[`S${electrodes[j].ids}`]) {
         boop = `${boop} ${elecToPin[`S${electrodes[j].ids}`]}`;
       }

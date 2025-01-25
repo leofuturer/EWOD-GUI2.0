8a9f39a3b2cbee6477e63edccd21f285dae96b6c
cdevadhar <cdevadhar@gmail.com>
2025-01-24 17:16:13 -0800

    added optional acknowledgement parameters in usbcomm

--- src/USBCommunication/USBCommunication.js
+++ src/USBCommunication/USBCommunication.js
@@ -76,7 +76,7 @@ export function isDeviceConnected() {
 
 // Set a list of pins to a given value (value is either 0 or 1)
 //   ex. setPin([9,10], 1) sets pins 9 and 10 to high
-export async function setPin(pins, value, reset = false) {
+export async function setPin(pins, value, reset = false, ack=false) {
   if (!EWODDevice) {
     console.log('Device not connected');
     return;
@@ -106,30 +106,33 @@ export async function setPin(pins, value, reset = false) {
     else EWODDeviceView[index] &= ~(1 << ((pin - 1) % 8));
   });
 
-  EWODDeviceView[0] = 0xAA;
+  if (ack) EWODDeviceView[0] = 0xAC;
+  else EWODDeviceView[0] = 0xAA;
 
   await EWODDevice.sendReport(0x00, EWODDeviceView);
 }
 
 // Sets EWOD's voltage
-export async function setV(voltage) {
+export async function setV(voltage, ack=false) {
   if (!EWODDevice) {
     console.log('Device not connected');
     return;
   }
 
-  EWODDeviceView[0] = 0xAA;
+  if (ack) EWODDeviceView[0] = 0xAC;
+  else EWODDeviceView[0] = 0xAA;
   EWODDeviceView[40] = voltage;
   await EWODDevice.sendReport(0x00, EWODDeviceView);
   setInterval(sendAck, 1000);
 }
 
 // Sets EWOD's frequency
-export async function setF(frequency) {
+export async function setF(frequency, ack=false) {
   const msb = frequency >> 8; // The highest bit is always 0 (max freq 10000 Hz)
   const lsb = frequency & 0xFF;
 
-  EWODDeviceView[0] = 0xAA;
+  if (ack) EWODDeviceView[0] = 0xAC;
+  else EWODDeviceView[0] = 0xAA;
   EWODDeviceView[36] = lsb;
   EWODDeviceView[37] = msb;
 
